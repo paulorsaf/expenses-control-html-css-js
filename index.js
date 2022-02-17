e4bd99daab882ba621309499e808b847e33be458
Paulo Alves <paulorsaf@gmail.com>
2022-02-17 19:27:52 +0100

    Validando o formulário de Login com Javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+function validateFields() {
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
+
+}
+
+function isEmailValid() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function isPasswordValid() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+}

e2ac7471a3466d2e34ceed51f07ca20ff2ac4495
Paulo Alves <paulorsaf@gmail.com>
2022-02-18 17:57:27 +0100

    Login: Mostrando mensagens de erro de formulário

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-function validateFields() {
+function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();
+}
+
+function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+} 
+
+function toggleEmailErrors() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        document.getElementById("email-required-error").style.display = "block";
+    } else {
+        document.getElementById("email-required-error").style.display = "none";
+    }
+    
+    if (validateEmail(email)) {
+        document.getElementById("email-invalid-error").style.display = "none";
+    } else {
+        document.getElementById("email-invalid-error").style.display = "block";
+    }
+}
+
+function togglePasswordErrors() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+       document.getElementById("password-required-error").style.display = "block";
+    } else {
+       document.getElementById("password-required-error").style.display = "none";
+    }
+}
+
+function toggleButtonsDisable() {
     const emailValid = isEmailValid();
     document.getElementById("recover-password-button").disabled = !emailValid;
 
     const passwordValid = isPasswordValid();
     document.getElementById("login-button").disabled = !emailValid || !passwordValid;
-
 }
 
 function isEmailValid() {

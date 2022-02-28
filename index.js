57b8661b809d029eb7840db3993f307541b2e1a8
Paulo Alves <paulorsaf@gmail.com>
2022-02-28 18:37:43 +0100

    Login por email e senha com firebase auth

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ function onChangePassword() {
 }
 
 function login() {
-    window.location.href = "pages/home/home.html";
+    firebase.auth().signInWithEmailAndPassword(
+        form.email().value, form.password().value
+    ).then(response => {
+        window.location.href = "pages/home/home.html";
+    }).catch(error => {
+        alert(getErrorMessage(error));
+    });
+}
+
+function getErrorMessage(error) {
+    if (error.code == "auth/user-not-found") {
+        return "Usuário nao encontrado";
+    }
+    return error.message;
 }
 
 function register() {

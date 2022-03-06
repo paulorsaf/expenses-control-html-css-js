9aa5fe7e76c33d79e73b395e2f2ffef655e0cc03
Paulo Alves <paulorsaf@gmail.com>
2022-03-06 17:56:55 +0100

    Validaçao da tela de registro

---
+++ pages/register/register.js
@@ -0,0 +1,66 @@
+function onChangeEmail() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+
+    toggleRegisterButtonDisable();
+}
+
+function onChangePassword() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+
+    form.passwordMinLengthError().style.display = password.length >= 6 ? "none" : "block";
+
+    validatePasswordsMatch();
+    toggleRegisterButtonDisable();
+}
+
+function onChangeConfirmPassword() {
+    validatePasswordsMatch();
+    toggleRegisterButtonDisable();
+}
+
+function validatePasswordsMatch() {
+    const password = form.password().value;
+    const confirmPassword = form.confirmPassword().value;
+
+    form.confirmPasswordDoesntMatchError().style.display =
+        password == confirmPassword ? "none" : "block";
+}
+
+function toggleRegisterButtonDisable() {
+    form.registerButton().disabled = !isFormValid();
+}
+
+function isFormValid() {
+    const email = form.email().value;
+    if (!email || !validateEmail(email)) {
+        return false;
+    }
+
+    const password = form.password().value;
+    if (!password || password.length < 6) {
+        return false;
+    }
+
+    const confirmPassword = form.confirmPassword().value;
+    if (password != confirmPassword) {
+        return false;
+    }
+
+    return true;
+}
+
+const form = {
+    confirmPassword: () => document.getElementById('confirmPassword'),
+    confirmPasswordDoesntMatchError: () => document.getElementById('password-doesnt-match-error'),
+    email: () => document.getElementById('email'),
+    emailInvalidError: () => document.getElementById('email-invalid-error'),
+    emailRequiredError: () => document.getElementById('email-required-error'),
+    password: () => document.getElementById('password'),
+    passwordMinLengthError: () => document.getElementById('password-min-length-error'),
+    passwordRequiredError: () => document.getElementById('password-required-error'),
+    registerButton: () => document.getElementById('register-button')
+}

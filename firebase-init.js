44489b9bf6d9764578697dad9212151f7ba54144
Paulo Alves <paulorsaf@gmail.com>
2022-03-08 14:05:48 +0100

    Registro de usuário com firebase authentication

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBSl2BdAd_DghcW5a5-qp8gFcK0nfuGCY",
+    authDomain: "controle-de-gastos-3e7f9.firebaseapp.com",
+    projectId: "controle-de-gastos-3e7f9",
+    storageBucket: "controle-de-gastos-3e7f9.appspot.com",
+    messagingSenderId: "1018055774945",
+    appId: "1:1018055774945:web:bb0b24d7c0a4992e16dda0"
+};
+firebase.initializeApp(firebaseConfig);
--- pages/register/register.js
+++ pages/register/register.js
@@ -22,6 +22,29 @@ function onChangeConfirmPassword() {
     toggleRegisterButtonDisable();
 }
 
+function register() {
+    showLoading();
+
+    const email = form.email().value;
+    const password = form.password().value;
+    firebase.auth().createUserWithEmailAndPassword(
+        email, password
+    ).then(() => {
+        hideLoading();
+        window.location.href = "../../pages/home/home.html";
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    })
+}
+
+function getErrorMessage(error) {
+    if (error.code == "auth/email-already-in-use") {
+        return "Email já está em uso";
+    }
+    return error.message;
+}
+
 function validatePasswordsMatch() {
     const password = form.password().value;
     const confirmPassword = form.confirmPassword().value;

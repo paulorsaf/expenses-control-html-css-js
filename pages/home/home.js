359933d72e4602f5efffb2c703b5906052f1706f
Paulo Alves <paulorsaf@gmail.com>
2022-03-14 11:33:57 +0100

    Logout do usuário

---
+++ pages/home/home.js
@@ -0,0 +1,7 @@
+function logout() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = "../../index.html";
+    }).catch(() => {
+        alert('Erro ao fazer logout');
+    })
+}

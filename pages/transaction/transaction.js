121714f9061f57dd3251b8ed823056a42b85c265
Paulo Alves <paulorsaf@gmail.com>
2022-03-31 11:39:17 +0200

    Salvando transaçao no firestore

--- pages/transaction/transaction.js
+++ pages/transaction/transaction.js
@@ -1,3 +1,37 @@
+function saveTransaction() {
+    showLoading();
+
+    const transaction = createTransaction();
+
+    firebase.firestore()
+        .collection('transactions')
+        .add(transaction)
+        .then(() => {
+            hideLoading();
+            window.location.href = "../home/home.html";
+        })
+        .catch(() => {
+            hideLoading();
+            alert('Erro ao salvar transaçao');
+        })
+}
+
+function createTransaction() {
+    return {
+        type: form.typeExpense().checked ? "expense" : "income",
+        date: form.date().value,
+        money: {
+            currency: form.currency().value,
+            value: parseFloat(form.value().value)
+        },
+        transactionType: form.transactionType().value,
+        description: form.description().value,
+        user: {
+            uid: firebase.auth().currentUser.uid
+        }
+    };
+}
+
 function onChangeDate() {
     const date = form.date().value;
     form.dateRequiredError().style.display = !date ? "block" : "none";
@@ -16,7 +50,6 @@ function onChangeValue() {
 
 function onChangeTransactionType() {
     const transactionType = form.transactionType().value;
-    console.log(transactionType)
     form.transactionTypeRequiredError().style.display = !transactionType ? "block" : "none";
 
     toggleSaveButtonDisable();
@@ -46,11 +79,14 @@ function isFormValid() {
 }
 
 const form = {
+    currency: () => document.getElementById('currency'),
     date: () => document.getElementById('date'),
+    description: () => document.getElementById('description'),
     dateRequiredError: () => document.getElementById('date-required-error'),
     saveButton: () => document.getElementById('save-button'),
     transactionType: () => document.getElementById('transaction-type'),
     transactionTypeRequiredError: () => document.getElementById('transaction-type-required-error'),
+    typeExpense: () => document.getElementById('expense'),
     value: () => document.getElementById('value'),
     valueRequiredError: () => document.getElementById('value-required-error'),
     valueLessOrEqualToZeroError: () => document.getElementById('value-less-or-equal-to-zero-error')

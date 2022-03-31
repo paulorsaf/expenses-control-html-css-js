4c81b2311e07c0692e6af47b590919743d11e604
Paulo Alves <paulorsaf@gmail.com>
2022-03-31 10:48:33 +0200

    Criando design da tela de transaçao

--- pages/home/home.js
+++ pages/home/home.js
@@ -12,6 +12,10 @@ firebase.auth().onAuthStateChanged(user => {
     }
 })
 
+function newTransaction() {
+    window.location.href = "../transaction/transaction.html";
+}
+
 function findTransactions(user) {
     showLoading();
     firebase.firestore()
